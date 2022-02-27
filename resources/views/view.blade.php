67a9524aa42c0f0cb3d5485be93d56661bbc9ff7
Hristijan <hristijanrahmanov@yahoo.com>
2022-02-27 15:21:50 +0100

    I've changed the view when you open a specific car.

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,18 +2,14 @@
 
 @section('content')
     <div class="container mt-3 mb-3">
-        <div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
-                    <div class="col">
-                        <div class="card">
-                                <img src="{{asset('storage/' . $car[0]->image)}}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h2 class="card-title">{{$car[0]->marka}}</h2>
-                                <h4 class="card-title">{{$car[0]->model}}</h4>
-                                <h6 class="card-title">{{$car[0]->godina}}</h6>
-                                <p class="card-text">This is a longer jovan NE JOVAN STOJCEV</p>
-                            </div>
-                        </div>
-                    </div>
+        <div class="card mb-3">
+            <img src="{{asset('storage/' . $car[0]->image)}}" style="height: 500px" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h2 class="card-title">{{$car[0]->marka}}</h2>
+                    <h4 class="card-title">{{$car[0]->model}}</h4>
+                    <h6 class="card-title">{{$car[0]->godina}}</h6>
+                    <p class="card-text">This is a longer jovan NE JOVAN STOJCEV</p>
+                </div>
         </div>
     </div>
 @endsection
