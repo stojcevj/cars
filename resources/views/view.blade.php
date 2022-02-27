b6d0fcd9eafc87e2ccc2f205c39be60b07cf1421
Jovan <stojcevjovan@hotmail.com>
2022-02-27 15:17:47 +0100

    ADD JOVAN STOJCEV

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -10,7 +10,7 @@
                                 <h2 class="card-title">{{$car[0]->marka}}</h2>
                                 <h4 class="card-title">{{$car[0]->model}}</h4>
                                 <h6 class="card-title">{{$car[0]->godina}}</h6>
-                                <p class="card-text">This is a longer jovan NE</p>
+                                <p class="card-text">This is a longer jovan NE JOVAN STOJCEV</p>
                             </div>
                         </div>
                     </div>
